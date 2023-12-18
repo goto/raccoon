1e33edeefd8c15ee7b2f8ef0195963abb3cacdc9
Siddhanta Rath <siddhanta.rath@gojek.com>
2023-12-18 20:57:37 +0530

    feat: enable tls on grpc server

--- services/grpc/service.go
+++ services/grpc/service.go
@@ -2,7 +2,9 @@ package grpc
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"google.golang.org/grpc/credentials"
 	"net"
 
 	pbgrpc "buf.build/gen/go/gotocompany/proton/grpc/go/gotocompany/raccoon/v1beta1/raccoonv1beta1grpc"
@@ -17,7 +19,7 @@ type Service struct {
 }
 
 func NewGRPCService(c collection.Collector) *Service {
-	server := grpc.NewServer()
+	server := newGRPCServer()
 	pbgrpc.RegisterEventServiceServer(server, &Handler{C: c})
 	return &Service{
 		s:         server,
@@ -40,4 +42,25 @@ func (*Service) Name() string {
 func (s *Service) Shutdown(context.Context) error {
 	s.s.GracefulStop()
 	return nil
+}
+
+func newGRPCServer() *grpc.Server {
+	if config.ServerGRPC.TLSEnabled {
+		return grpc.NewServer(grpc.Creds(loadTLSCredentials()))
+	}
+	return grpc.NewServer()
+}
+
+func loadTLSCredentials() credentials.TransportCredentials {
+	serverCert, err := tls.LoadX509KeyPair(config.ServerGRPC.TLSCertPath, config.ServerGRPC.TLSPublicKey)
+	if err != nil {
+		panic("failed to load TLS credentials to start grpc server with TLS")
+	}
+
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.NoClientCert,
+	}
+
+	return credentials.NewTLS(config)
 }
