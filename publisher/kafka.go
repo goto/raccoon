93e32a92f845d6c52e0b864db0278e078f1724e1
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2026-03-13 15:21:12 +0700

    feat: add app_version and platform tags for clickstream_data_loss

--- publisher/kafka.go
+++ publisher/kafka.go
@@ -138,10 +138,11 @@ func (pr *Kafka) ProduceBulk(
 				errorTag = "KAFKA_ERROR"
 			}
 
-			metrics.Increment("clickstream_data_loss", fmt.Sprintf("reason=%s,event_name=%s,product=%s,conn_group=%s",
-				errorTag, event.EventName, strings.ReplaceAll(strings.ToLower(event.Product), "_", ""), connGroup,
-			))
+			tags := fmt.Sprintf("reason=%s,event_name=%s,product=%s,conn_group=%s,app_version=%s,platform=%s",
+				errorTag, event.EventName, strings.ReplaceAll(strings.ToLower(event.Product), "_", ""), connGroup, event.AppVersion, event.Platform,
+			)
 
+			metrics.Increment("clickstream_data_loss", tags)
 			continue
 		}
 
@@ -166,9 +167,11 @@ func (pr *Kafka) ProduceBulk(
 			metrics.Decrement("kafka_messages_delivered_total", fmt.Sprintf("success=true,conn_group=%s,event_type=%s", connGroup, eventType))
 			metrics.Increment("kafka_messages_delivered_total", fmt.Sprintf("success=false,conn_group=%s,event_type=%s", connGroup, eventType))
 			metrics.Increment("kafka_error", fmt.Sprintf("type=%s,event_type=%s,conn_group=%s", "delivery_failed", eventType, connGroup))
-			metrics.Increment("clickstream_data_loss", fmt.Sprintf("reason=%s,event_name=%s,product=%s,conn_group=%s",
-				"KAFKA_ERROR", event.EventName, strings.ReplaceAll(strings.ToLower(event.Product), "_", ""), connGroup,
-			))
+
+			tags := fmt.Sprintf("reason=%s,event_name=%s,product=%s,conn_group=%s,app_version=%s,platform=%s",
+				"KAFKA_ERROR", event.EventName, strings.ReplaceAll(strings.ToLower(event.Product), "_", ""), connGroup, event.AppVersion, event.Platform,
+			)
+			metrics.Increment("clickstream_data_loss", tags)
 			errors[order] = m.TopicPartition.Error
 		} else {
 			startTimeEvent := startTimeEvents[order]
