1cef953b6b4ba0950134d74f756238bd26b15853
Muhammad Fadli Gunardi <muhammad.gunardi@gojek.com>
2026-07-14 16:22:12 +0700

    chore: add config log

--- config/load.go
+++ config/load.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/goto/raccoon/logger"
 	"github.com/spf13/viper"
 )
 
@@ -37,4 +38,32 @@ func Load() {
 	deserializationConfigLoader()
 	compassConfigLoader()
 	metadataLayerConfigLoader()
+}
+
+func LogConfig() {
+	logger.Info("Config: Log level: " + Log.Level)
+	logger.Infof("Config: Server: %+v", Server)
+	logger.Infof("Config: ServerWs: %+v", ServerWs)
+	logger.Infof("Config: ServerGRPC: %+v", ServerGRPC)
+
+	mqttSafe := ServerMQTT
+	mqttSafe.AuthConfig.Password = "[REDACTED]"
+	logger.Infof("Config: ServerMQTT: %+v", mqttSafe)
+
+	logger.Infof("Config: Worker: %+v", Worker)
+	logger.Infof("Config: Event: %+v", Event)
+	logger.Infof("Config: PublisherKafka: %+v", PublisherKafka)
+	logger.Infof("Config: MetricStatsd: %+v", MetricStatsd)
+	logger.Infof("Config: EventDistribution: %+v", EventDistribution)
+	logger.Infof("Config: PolicyCfg: %+v", PolicyCfg)
+
+	redisSafe := RedisCfg
+	redisSafe.Password = "[REDACTED]"
+	logger.Infof("Config: RedisCfg: %+v", redisSafe)
+
+	logger.Infof("Config: DedupCfg: %+v", DedupCfg)
+	logger.Infof("Config: StencilCfg: %+v", StencilCfg)
+	logger.Infof("Config: DeserializationCfg: %+v", DeserializationCfg)
+	logger.Infof("Config: CompassCfg: %+v", CompassCfg)
+	logger.Infof("Config: MetadataLayerCfg: %+v", MetadataLayerCfg)
 }
